f65ae37ca3fd08719fefdc9f32360c42ce776023
anupomm <anupomhstu@gmail.com>
2024-02-10 14:58:42 +0600

    splash screen created

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'application/app.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const CraftyBay());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
