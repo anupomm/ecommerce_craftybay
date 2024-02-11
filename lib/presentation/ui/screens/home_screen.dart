6562aee99d8d0bf7d5b0c4c7593b7c480d3060ef
anupomm <anupomhstu@gmail.com>
2024-02-11 19:09:42 +0600

    home screen app bar and slider has been added

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -1,9 +1,84 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:ecommerce/presentation/ui/utility/app_colors.dart';
+import 'package:ecommerce/presentation/ui/utility/image_assets.dart';
 import 'package:flutter/material.dart';
-class HomeScreen extends StatelessWidget {
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../widgets/circular_icon_button.dart';
+import '../widgets/home/home_slider.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Row(
+          children: [
+            SvgPicture.asset(ImageAssets.craftyBayNavLogoSVG),
+            const Spacer(),
+            CircularIconButton(
+              icon: Icons.person,
+              onTap: () {},
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            CircularIconButton(
+              icon: Icons.call,
+              onTap: () {},
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            CircularIconButton(
+              icon: Icons.notifications_none,
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.search),
+                  hintText: 'Search',
+                  fillColor: Colors.grey.shade200,
+                  filled: true,
+                  border: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const HomeSlider(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
+
---
+++ lib/presentation/ui/widgets/circular_icon_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class CircularIconButton extends StatelessWidget {
+  const CircularIconButton({
+    super.key, required this.icon, required this.onTap,
+  });
+
+  final IconData icon;
+  final VoidCallback onTap;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(50),
+      child: CircleAvatar(
+        radius: 14,
+        backgroundColor: Colors.grey.shade200,
+        child: Icon(icon, color: Colors.grey, size: 16,),
+      ),
+    );
+  }
+}
